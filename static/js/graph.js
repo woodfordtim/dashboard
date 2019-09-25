91fa7eee743689b4ed24f131246203bf722e1601
Ubuntu <ubuntu@ip-172-31-93-68.ec2.internal>
2019-09-25 23:02:16 +0000

    phd scatter

--- static/js/graph.js
+++ static/js/graph.js
@@ -7,6 +7,7 @@ function makeGraphs(error, salaryData) {
     
     salaryData.forEach(function(d){
         d.salary = parseInt(d.salary);
+        d.yrs_since_phd = parseInt(d["yrs.since.phd"]);
         d.yrs_service = parseInt(d["yrs.service"]);
     })
     
@@ -20,6 +21,7 @@ function makeGraphs(error, salaryData) {
     show_rank_distribution(ndx);
     
     show_service_to_salary_correlation(ndx);
+    show_phd_to_salary_correlation(ndx);
     
     dc.renderAll();
 }
@@ -209,6 +211,7 @@ function show_service_to_salary_correlation(ndx) {
         .brushOn(false)
         .symbolSize(8)
         .clipPadding(10)
+        .yAxisLabel("Salary")
         .xAxisLabel("Years Of Service")
         .title(function(d) {
             return d.key[2] + " earned " + d.key[1];
@@ -219,5 +222,41 @@ function show_service_to_salary_correlation(ndx) {
         .colors(genderColors)
         .dimension(experienceDim)
         .group(experienceSalaryGroup)
+        .margins({top: 10, right: 50, bottom: 75, left: 75});
+}
+
+function show_phd_to_salary_correlation(ndx) {
+    
+    var genderColors = d3.scale.ordinal()
+        .domain(["Female", "Male"])
+        .range(["pink", "blue"]);
+    
+    var pDim = ndx.dimension(dc.pluck("yrs_since_phd"));
+    var phdDim = ndx.dimension(function(d) {
+       return [d.yrs_since_phd, d.salary, d.rank, d.sex];
+    });
+    var phdSalaryGroup = phdDim.group();
+    
+    var minPhd = pDim.bottom(1)[0].yrs_since_phd;
+    var maxPhd = pDim.top(1)[0].yrs_since_phd;
+    
+    dc.scatterPlot("#phd-salary")
+        .width(800)
+        .height(400)
+        .x(d3.scale.linear().domain([minPhd, maxPhd]))
+        .brushOn(false)
+        .symbolSize(8)
+        .clipPadding(10)
+        .yAxisLabel("Salary")
+        .xAxisLabel("Years Since PhD")
+        .title(function(d) {
+            return d.key[2] + " earned " + d.key[1];
+        })
+        .colorAccessor(function (d) {
+            return d.key[3];
+        })
+        .colors(genderColors)
+        .dimension(phdDim)
+        .group(phdSalaryGroup)
         .margins({top: 10, right: 50, bottom: 75, left: 75});
 }
