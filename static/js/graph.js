a47bdef40f556bad844ace47b8bdfb100d1a0765
Ubuntu <ubuntu@ip-172-31-93-68.ec2.internal>
2019-09-29 14:16:15 +0000

    Completed dashboard

--- static/js/graph.js
+++ static/js/graph.js
@@ -80,8 +80,8 @@ function show_gender_balance(ndx) {
     var group = dim.group();
     
     dc.barChart("#gender-balance")
-        .width(400)
-        .height(300)
+        .width(350)
+        .height(250)
         .margins({top: 10, right: 50, bottom: 30, left: 50})
         .dimension(dim)
         .group(group)
@@ -122,8 +122,8 @@ function show_average_salary(ndx) {
     var averageSalaryByGender = dim.group().reduce(add_item, remove_item, initialise);
 
     dc.barChart("#average-salary")
-        .width(400)
-        .height(300)
+        .width(350)
+        .height(250)
         .margins({top: 10, right: 50, bottom: 30, left: 50})
         .dimension(dim)
         .group(averageSalaryByGender)
@@ -169,8 +169,8 @@ function show_rank_distribution(ndx) {
     var assocProfByGender = rankByGender(dim, "AssocProf");
     
     dc.barChart("#rank-distribution")
-        .width(400)
-        .height(300)
+        .width(350)
+        .height(250)
         .dimension(dim)
         .group(profByGender, "Prof")
         .stack(asstProfByGender, "Asst Prof")
@@ -184,6 +184,7 @@ function show_rank_distribution(ndx) {
         })
         .x(d3.scale.ordinal())
         .xUnits(dc.units.ordinal)
+        .xAxisLabel("Gender")
         .legend(dc.legend().x(320).y(20).itemHeight(15).gap(5))
         .margins({top: 10, right: 100, bottom: 30, left: 30});
 }
